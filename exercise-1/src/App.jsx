9a8fe7198b099a5b9129a57731ee930c49f3dbd2
THAL SOTHARITH <yuththarith22@gmail.com>
2026-03-12 13:34:36 +0700

    done ex1

--- exercise-1/src/App.jsx
+++ exercise-1/src/App.jsx
@@ -29,6 +29,7 @@ function App() {
       <h1>{getTitle()}</h1>
       <button onClick={onSunClick}>Sunny Time</button>
       <button onClick={onRainClick}>Rain Time</button>
+      
     </main>
   );
 }
